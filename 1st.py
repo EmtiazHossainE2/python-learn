8a3bcae09b1251dd6c928d76fb2a5a676294c2f6
EmtiazHossainE2 <emtiazemon310@gmail.com>
2024-01-03 19:26:30 +0600

    task done

--- 1st.py
+++ 1st.py
@@ -2,3 +2,7 @@ name = 'Emtiaz'
 
 print(name)
 print(type(name))
+
+x = ["apple", "banana", "cherry"]
+
+print(x)
---
+++ Task/task1.py
@@ -0,0 +1,33 @@
+# 1
+print('Hello Python')
+
+# 2 
+input = -1
+
+if(input == 0):
+  print('Value is Zero')
+elif(input > 0 ):
+  print('Value is positive')
+else:
+  print('Value is negative')
+
+#3 
+myRoll = -1 
+
+if(myRoll % 2 == 0 & myRoll > 0):
+  print('My roll is even .')
+elif(myRoll % 2 != 0 & myRoll > 0):
+  print('My roll is odd')
+else:
+  print('Invalid roll')
+
+
+#4 
+myAge = 20
+yourAge = 23
+if yourAge > myAge:
+  print("You are big boss")
+elif yourAge == myAge:
+  print("We are same bro")
+else:
+  print("I am big boss")
